b32d04af20a1a2bbc429d4ba48b569cbe57c11ad
feven@daniel <danielmesfin331@gmail.com>
2024-01-14 14:56:28 +0300

    the all home page

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,52 +1,114 @@
-
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                  title: Text('Hello Ahad!', style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.white
+                  )),
+                  subtitle: Text('Good Morning', style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: Colors.white54
+                  )),
+                  trailing: const CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('images/user.JPG'),
+                  ),
+                ),
+                const SizedBox(height: 30)
+              ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(200)
+                )
+              ),
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 40,
+                mainAxisSpacing: 30,
+                children: [
+                  itemDashboard('Videos', CupertinoIcons.play_rectangle, Colors.deepOrange),
+                  itemDashboard('Analytics', CupertinoIcons.graph_circle, Colors.green),
+                  itemDashboard('Audience', CupertinoIcons.person_2, Colors.purple),
+                  itemDashboard('Comments', CupertinoIcons.chat_bubble_2, Colors.brown),
+                  itemDashboard('Revenue', CupertinoIcons.money_dollar_circle, Colors.indigo),
+                  itemDashboard('Upload', CupertinoIcons.add_circled, Colors.teal),
+                  itemDashboard('About', CupertinoIcons.question_circle, Colors.blue),
+                  itemDashboard('Contact', CupertinoIcons.phone, Colors.pinkAccent),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 20)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
+
+  itemDashboard(String title, IconData iconData, Color background) => Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        BoxShadow(
+          offset: const Offset(0, 5),
+          color: Theme.of(context).primaryColor.withOpacity(.2),
+          spreadRadius: 2,
+          blurRadius: 5
+        )
+      ]
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: background,
+            shape: BoxShape.circle,
+          ),
+          child: Icon(iconData, color: Colors.white)
+        ),
+        const SizedBox(height: 8),
+        Text(title.toUpperCase(), style: Theme.of(context).textTheme.titleMedium)
+      ],
+    ),
+  );
 }
+
+
+
